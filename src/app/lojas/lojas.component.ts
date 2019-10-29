cfe1630da6657ca15888fbea4d081d3be16f992a
Anderson <anderson.henrique.ma9@gmail.com>
2019-10-28 21:53:42 -0300

    final da noite

--- src/app/lojas/lojas.component.ts
+++ src/app/lojas/lojas.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-restaurantes',
-  templateUrl: './lojas.component.html',
-  styleUrls: ['./lojas.component.scss']
+  selector: "app-restaurantes",
+  templateUrl: "./lojas.component.html",
+  styleUrls: ["./lojas.component.scss"]
 })
 export class LojasComponent implements OnInit {
+  listaLojas: object[] = [];
+  imgLogo;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  }
+    for (let i = 0; i < 21; i++) {
+      // console.log(i)
+      const randomNumber = Math.floor(Math.random() * 100);
+      console.log(randomNumber);
+      this.listaLojas.push({
+        nome: `Loja ${i}`,
+        logo: `https://picsum.photos/id/${randomNumber}/200/200`
+      });
+    }
 
+    console.log(this.listaLojas);
+  }
 }
